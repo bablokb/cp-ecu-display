6e901fb44fe44d2b410d678f8835c0d747a183f1
Bernhard Bablok <bablokb@gmx.de>
2025-10-16 08:43:42 +0200

    implement mock-data

--- src/ecu_display_dataprovider.py
+++ src/ecu_display_dataprovider.py
@@ -7,6 +7,7 @@
 # Website: https://github.com/bablokb/cp-ecu-display
 # ----------------------------------------------------------------------------
 
+import time
 from settings import app_config
 
 # --- main data-provider class   ---------------------------------------------
@@ -17,6 +18,7 @@ class DataProvider:
     self._debug = getattr(app_config, "debug", False)
     self._wifi  = None
     self._inverter = None
+    self._mock_index = 0
 
   # --- print debug-message   ------------------------------------------------
 
@@ -44,18 +46,39 @@ class DataProvider:
       port=getattr(app_config, "remote_port", 8899),
       debug=self._debug)
 
+  # --- return mock data   ---------------------------------------------------
+
+  def _get_mock_data(self):
+    """ create mock data """
+
+    data = {}
+    data['current_power'] = app_config.mock_data[self._mock_index]
+    self._mock_index = (self._mock_index+1) % len(app_config.mock_data)
+
+    # use "now" for timestamp
+    ts = time.localtime()
+    data['timestamp'] = "%04d-%02d-%02d %02d:%02d:%02d" % (
+        ts.tm_year,ts.tm_mon,ts.tm_mday,
+        ts.tm_hour,ts.tm_min,ts.tm_sec
+        )
+    return data
+
   # --- query data   ---------------------------------------------------------
 
   def update_data(self,data):
     """ callback for App: query data and update data-object """
 
-    if not self._inverter:
+    if not self._inverter and not app_config.mock:
       self._create_inverter()
     
     # read data from socket
     self.msg("DataProvider: reading data...")
-    ecu_data = self._inverter.asdict()                 # this will auto-update
-    self.msg(f"DataProvider: {data=}")
+    if app_config.mock:
+      self.msg("DataProvider: using mock data...")
+      ecu_data = self._get_mock_data()
+    else:
+      ecu_data = self._inverter.asdict()                 # this will auto-update
+    self.msg(f"DataProvider: {ecu_data=}")
 
     # update
     data.update(ecu_data)
